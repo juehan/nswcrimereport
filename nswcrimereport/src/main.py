4564f5cb3f1e769a32f20ac2b9a2fe411cb69aaa
John Lee <to.johnny.lee@gmail.com>
2012-02-18 02:26:11 +1100

    beautify

--- nswcrimereport/src/main.py
+++ nswcrimereport/src/main.py
@@ -2,32 +2,29 @@
 
 import sys
 import html
-from crawler import GenerateNewsList
+from crawler import generate_news_list
 from crimegmaps import CrimeGMaps
 
 
 def main():
-  crimeList = GenerateNewsList()
-  for crime in crimeList:
-    for x in range(5):
-      print "crime[" + str(x) +"] : ", str(crime[x])
-    print "----------------------------------------------------------"
+    crimeList = generate_news_list()
+    for crime in crimeList:
+        for x in range(5):
+            print "crime[" + str(x) +"] : ", str(crime[x])
+        print "----------------------------------------------------------"
     
-  header = html.getHeader()
-  
-  gmap = CrimeGMaps(crimeList)
-  
-  option = gmap.optionStr()
-  latlng = gmap.latLngStr()
-  places = gmap.placeStr()
-  infoWindow = gmap.infoWindowStr()
-  body = option + latlng + places + infoWindow
+    header = html.getHeader()
+    gmap = CrimeGMaps(crimeList)
+    option = gmap.optionStr()
+    latlng = gmap.latLngStr()
+    places = gmap.placeStr()
+    infoWindow = gmap.infoWindowStr()
+    body = option + latlng + places + infoWindow
+    footer = html.getFooter()
     
-  footer = html.getFooter()
-
-  html.makeHTML(header, body, footer)
-  print "NSW crime news page updated"
+    html.makeHTML(header, body, footer)
+    print "NSW crime news page updated"
 
 if __name__ == '__main__':
-  status = main()
-  sys.exit(status)
+    status = main()
+    sys.exit(status)
