e8d500fd76b482a3086bad6cf7ccb5024ee8206c
John Lee <to.johnny.lee@gmail.com>
2012-02-18 02:26:48 +1100

    Update as per Pylint and cover coverage warnings

--- nswcrimereport/src/crawler.py
+++ nswcrimereport/src/crawler.py
@@ -1,59 +1,64 @@
 # -*- coding:utf-8 -*-
-
+'''Module to crawl data from nsw police website'''
 import urllib2
 from BeautifulSoup import BeautifulSoup
 from geopy import geocoders
 import time
 import re
 
-def HasDash(mystr):
-  location = mystr.find('-')
-  if location is not -1:
-    if mystr[location+1] == " ":
-      return True
-  return False
 
-def IsValidLocation(rawlocation):
-  locationlist =  rawlocation.split(' ')
-  if len(locationlist) > 2:
-    return False
-  else:
-    return True
+def has_dash(mystr):
+    '''check whether string has dash ''' 
+    location = mystr.find('-')
+    if location is not -1:
+        if mystr[location+1] == " ":
+            return True
+        return False
+
+def is_valid_location(rawlocation):
+    '''return false if location is longer than 3 words'''
+    locationlist =  rawlocation.split(' ')
+    if len(locationlist) > 2:
+        return False
+    else:
+        return True
   
 # \u2010 : HYPHEN
 # \u2013 : EN DASH
 # \u2014 : EM DASH
-def AnalyzeTitle(title):
-  hasDash = HasDash(title)
-  if hasDash:
-    location = title.find('-')
-    strCrime = title[:location - 1];
-    if IsValidLocation(title[location + 2:]):
-      strLocation = title[location + 2:] + ", NSW, Australia"
+def analyze_title(title):
+    ''' Analyze title'''
+    hasdash = has_dash(title)
+    if hasdash:
+        location = title.find('-')
+        strcrime = title[:location - 1]
+        if is_valid_location(title[location + 2:]):
+            strlocation = title[location + 2:] + ", NSW, Australia"
+        else:
+            strlocation = ""
+        return (strcrime, strlocation)
     else:
-      strLocation = ""
-    return (strCrime, strLocation)
-  else:
-    return ("", "")
+        return ("", "")
 
   
-def GetLatLngList(location):
-  LatLng = []
-  g = geocoders.Google(resource = 'maps')
+def get_latlng_list(location):
+    '''   get latitude and longitude    '''
+    latlng = []
+    gmaps = geocoders.Google(resource = 'maps')
   
-  try:    
-    places, (lat, lng) = g.geocode(location)
-    time.sleep(0.1)
-    LatLng.append("%.5f" % lat)
-    LatLng.append("%.5f" % lng)
-  except Exception as e:
+    try:    
+        places, (lat, lng) = gmaps.geocode(location)
+        time.sleep(0.1)
+        latlng.append("%.5f" % lat)
+        latlng.append("%.5f" % lng)
+    except Exception as ex:
 #    x = "Didn't find exactly one placemark!"
 #    if str(e).find(x) >= 0:
 #      places, (lat, lng) = g.geocode(location, exactly_one=False)
 #      #places = g.geocode(location, exactly_one=False)
 #      time.sleep(0.1)
-#      LatLng.append("%.5f" % places[1][0])
-#      LatLng.append("%.5f" % places[1][1])
+#      latlng.append("%.5f" % places[1][0])
+#      latlng.append("%.5f" % places[1][1])
 #      log = open('logfile.txt', 'a')
 #      s = 'location: {0}, Exception: {1}'.format(location, e)
 #      log.write('%s\n' %s)
@@ -62,43 +67,43 @@ def GetLatLngList(location):
 #      log = open('logfile.txt', 'a')
 #      s = 'location: {0}, Exception: {1}'.format(location, e)
 #      log.write('%s\n' %s)
-#      log.close()
+#      log.close()  
+        log = open('logfile.txt', 'a')
+        ss_ = 'location: {0}, Exception: {1}'.format(location, ex)
+        log.write('%s\n' %ss_)
+        log.close()
+
+    return latlng
   
-    log = open('logfile.txt', 'a')
-    s = 'location: {0}, Exception: {1}'.format(location, e)
-    log.write('%s\n' %s)
-    log.close()
 
-  return LatLng
-  
+NEWS_SITE = 'http://m.police.nsw.gov.au/news'
 
-newsSite = 'http://m.police.nsw.gov.au/news'
-
-def GenerateNewsList():
-  html_source = urllib2.urlopen(newsSite)
-  soup = BeautifulSoup(html_source.read())
-  newsList = []
-  for i in range (2,32):
-    htmltext = soup('li')[i]
-    weblink = htmltext('a')[0]['href']
-    info = htmltext.findAllNext(text = True)
-    date = info[2]
-    strCrime, strLocation = AnalyzeTitle(info[1])
-    latLng = GetLatLngList(strLocation)
+def generate_news_list():
+    '''generate news list'''
+    html_source = urllib2.urlopen(NEWS_SITE)
+    soup = BeautifulSoup(html_source.read())
+    newslist = []
+    for i in range (2, 32):
+        htmltext = soup('li')[i]
+        weblink = htmltext('a')[0]['href']
+        info = htmltext.findAllNext(text = True)
+        date = info[2]
+        strcrime, strlocation = analyze_title(info[1])
+        latlang = get_latlng_list(strlocation)
      
-    if strCrime and strLocation and len(latLng) > 0:
-      news = []
-      if re.search('"', strCrime) or re.search("'", strCrime) :
-        strCrimeEscaped = re.escape(strCrime)
-        news.append(strCrimeEscaped)
-      else: 
-        news.append(strCrime)
-      news.append(strLocation)
-      news.append(weblink)      
-      news.append(date)
-      news.append(latLng)
-      newsList.append(news)
-  return newsList
+        if strcrime and strlocation and len(latlang) > 0:
+            news = []
+            if re.search('"', strcrime) or re.search("'", strcrime) :
+                strcrime_escaped = re.escape(strcrime)
+                news.append(strcrime_escaped)
+            else:
+                news.append(strcrime)
+            news.append(strlocation)
+            news.append(weblink)
+            news.append(date)
+            news.append(latlang)
+            newslist.append(news)
+    return newslist
 
 
 
